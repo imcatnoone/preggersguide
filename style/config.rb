18d5386035bb341a83d160556459fd4ddc9098cb
Ashley Sullivan <ashley@ashleysullivan.co>
2017-03-09 12:48:01 -0600

    Compiled SASS

--- style/config.rb
+++ style/config.rb
@@ -5,11 +5,12 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "stylesheets"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
+images_dir = "../assets/images"
+javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
