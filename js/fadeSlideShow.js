9d00b98f68cbbb6e2acaeba1e04b882d3aa9f9d7
Ashley Sullivan <ashley@ashleysullivan.co>
2017-04-26 20:04:56 -0500

    Make indentations consistent across project

--- js/fadeSlideShow.js
+++ js/fadeSlideShow.js
@@ -22,167 +22,167 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
  */
 
 jQuery.fn.fadeSlideShow = function(options) {
-	return this.each(function(){
-		settings = jQuery.extend({
-			// width: 640, // default width of the slideshow
-			height: 300, // default height of the slideshow
-			speed: 'slow', // default animation transition speed
-			interval: 3000, // default interval between image change
-			Active: 'fssActive', // default class for active stat
-			ListLiActive: 'fssActive', // default class for active state in the controll list
-			addListToId: false, // add the controll list to special id in your code - default false
-			autoplay: true, // autoplay the slideshow
-			beforeSlide: function(){}, // function to call before going to the next slide
-			afterSlide: function(){} // function to call after going to the next slide
-	 	}, options);
+  return this.each(function(){
+    settings = jQuery.extend({
+      // width: 640, // default width of the slideshow
+      height: 300, // default height of the slideshow
+      speed: 'slow', // default animation transition speed
+      interval: 3000, // default interval between image change
+      Active: 'fssActive', // default class for active stat
+      ListLiActive: 'fssActive', // default class for active state in the controll list
+      addListToId: false, // add the controll list to special id in your code - default false
+      autoplay: true, // autoplay the slideshow
+      beforeSlide: function(){}, // function to call before going to the next slide
+      afterSlide: function(){} // function to call after going to the next slide
+    }, options);
 
-		// set style for wrapper element
-		jQuery(this).css({
-			width: settings.width,
-			height: settings.height,
-			position: 'relative',
-			overflow: 'hidden'
-		});
+    // set style for wrapper element
+    jQuery(this).css({
+      width: settings.width,
+      height: settings.height,
+      position: 'relative',
+      overflow: 'hidden'
+    });
 
-		// set styles for child element
-		jQuery('> *',this).css({
-			position: 'absolute',
-			// width: settings.width,
-			height: settings.height
-		});
+    // set styles for child element
+    jQuery('> *',this).css({
+      position: 'absolute',
+      // width: settings.width,
+      height: settings.height
+    });
 
-		// count number of slides
-		var Slides = jQuery('> *', this).length;
-		Slides = Slides - 1;
-		var ActSlide = Slides;
-		// Set jQuery Slide short var
-		var jQslide = jQuery('> *', this);
-		// save this
-		var fssThis = this;
-		var intval = false;
-		// Set active class to current active slide.
-		jQslide.removeClass(settings.Active);
-		jQslide.eq(ActSlide).addClass(settings.Active);
-		var autoplay = function(){
-			intval = setInterval(function(){
-				settings.beforeSlide();
-				jQslide.eq(ActSlide).fadeOut(settings.speed);
+    // count number of slides
+    var Slides = jQuery('> *', this).length;
+    Slides = Slides - 1;
+    var ActSlide = Slides;
+    // Set jQuery Slide short var
+    var jQslide = jQuery('> *', this);
+    // save this
+    var fssThis = this;
+    var intval = false;
+    // Set active class to current active slide.
+    jQslide.removeClass(settings.Active);
+    jQslide.eq(ActSlide).addClass(settings.Active);
+    var autoplay = function(){
+      intval = setInterval(function(){
+        settings.beforeSlide();
+        jQslide.eq(ActSlide).fadeOut(settings.speed);
 
-				// if list is on change the active class
-				if(settings.ListElement){
-					var setActLi = (Slides - ActSlide) + 1;
-					if(setActLi > Slides){setActLi=0;}
-					jQuery('#'+settings.ListElement+' li').removeClass(settings.ListLiActive);
-					jQuery('#'+settings.ListElement+' li').eq(setActLi).addClass(settings.ListLiActive);
-				}
+        // if list is on change the active class
+        if(settings.ListElement){
+          var setActLi = (Slides - ActSlide) + 1;
+          if(setActLi > Slides){setActLi=0;}
+          jQuery('#'+settings.ListElement+' li').removeClass(settings.ListLiActive);
+          jQuery('#'+settings.ListElement+' li').eq(setActLi).addClass(settings.ListLiActive);
+        }
 
-				if(ActSlide <= 0){
-					jQslide.fadeIn(settings.speed);
-					ActSlide = Slides;
-				}else{
-					ActSlide = ActSlide - 1;
-				}
+        if(ActSlide <= 0){
+          jQslide.fadeIn(settings.speed);
+          ActSlide = Slides;
+        }else{
+          ActSlide = ActSlide - 1;
+        }
 
-				// Set active class to current active slide.
-				jQslide.removeClass(settings.Active);
-				jQslide.eq(ActSlide).addClass(settings.Active);
+        // Set active class to current active slide.
+        jQslide.removeClass(settings.Active);
+        jQslide.eq(ActSlide).addClass(settings.Active);
 
-				settings.afterSlide();
+        settings.afterSlide();
 
-			}, settings.interval);
+      }, settings.interval);
 
-			if(settings.PlayPauseElement){
-				jQuery('#'+settings.PlayPauseElement).html(settings.PauseText);
-			}
-		};
+      if(settings.PlayPauseElement){
+        jQuery('#'+settings.PlayPauseElement).html(settings.PauseText);
+      }
+    };
 
-		var stopAutoplay = function(){
-			clearInterval(intval);
-			intval = false;
-			if(settings.PlayPauseElement){
-				jQuery('#'+settings.PlayPauseElement).html(settings.PlayText);
-			}
-		};
+    var stopAutoplay = function(){
+      clearInterval(intval);
+      intval = false;
+      if(settings.PlayPauseElement){
+        jQuery('#'+settings.PlayPauseElement).html(settings.PlayText);
+      }
+    };
 
-		var jumpTo = function(newIndex){
-			if(newIndex < 0){newIndex = Slides;}
-			else if(newIndex > Slides){newIndex = 0;}
-			settings.beforeSlide();
-			if( newIndex >= ActSlide ){
-				jQuery('> *:lt('+(newIndex+1)+')', fssThis).fadeIn(settings.speed);
-			}else if(newIndex <= ActSlide){
-				jQuery('> *:gt('+newIndex+')', fssThis).fadeOut(settings.speed);
-			}
+    var jumpTo = function(newIndex){
+      if(newIndex < 0){newIndex = Slides;}
+      else if(newIndex > Slides){newIndex = 0;}
+      settings.beforeSlide();
+      if( newIndex >= ActSlide ){
+        jQuery('> *:lt('+(newIndex+1)+')', fssThis).fadeIn(settings.speed);
+      }else if(newIndex <= ActSlide){
+        jQuery('> *:gt('+newIndex+')', fssThis).fadeOut(settings.speed);
+      }
 
-			// set the active slide
-			ActSlide = newIndex;
+      // set the active slide
+      ActSlide = newIndex;
 
-			// Set active class to current active slide.
-			jQslide.removeClass(settings.Active);
-			jQslide.eq(ActSlide).addClass(settings.Active);
+      // Set active class to current active slide.
+      jQslide.removeClass(settings.Active);
+      jQslide.eq(ActSlide).addClass(settings.Active);
 
-			if(settings.ListElement){
-				// set active
-				jQuery('#'+settings.ListElement+' li').removeClass(settings.ListLiActive);
-				jQuery('#'+settings.ListElement+' li').eq((Slides-newIndex)).addClass(settings.ListLiActive);
-			}
-			settings.afterSlide();
-		};
+      if(settings.ListElement){
+        // set active
+        jQuery('#'+settings.ListElement+' li').removeClass(settings.ListLiActive);
+        jQuery('#'+settings.ListElement+' li').eq((Slides-newIndex)).addClass(settings.ListLiActive);
+      }
+      settings.afterSlide();
+    };
 
-		// if list is on render it
-		if(settings.ListElement){
-			var i=0;
-			var li = '';
-			while(i<=Slides){
-				if(i==0){
-					li = li+'<li class="'+settings.ListLi+i+' '+settings.ListLiActive+'"><a href="#">'+(i+1)+'<\/a><\/li>';
-				}else{
-					li = li+'<li class="'+settings.ListLi+i+'"><a href="#">'+(i+1)+'<\/a><\/li>';
-				}
-				i++;
-			}
-			var List = '<ul id="'+settings.ListElement+'">'+li+'<\/ul>';
+    // if list is on render it
+    if(settings.ListElement){
+      var i=0;
+      var li = '';
+      while(i<=Slides){
+        if(i==0){
+          li = li+'<li class="'+settings.ListLi+i+' '+settings.ListLiActive+'"><a href="#">'+(i+1)+'<\/a><\/li>';
+        }else{
+          li = li+'<li class="'+settings.ListLi+i+'"><a href="#">'+(i+1)+'<\/a><\/li>';
+        }
+        i++;
+      }
+      var List = '<ul id="'+settings.ListElement+'">'+li+'<\/ul>';
 
-			// add list to a special id or append after the slideshow
-			if(settings.addListToId){
-				jQuery('#'+settings.addListToId).append(List);
-			}else{
-				jQuery(this).after(List);
-			}
+      // add list to a special id or append after the slideshow
+      if(settings.addListToId){
+        jQuery('#'+settings.addListToId).append(List);
+      }else{
+        jQuery(this).after(List);
+      }
 
-			jQuery('#'+settings.ListElement+' a').bind('click', function(){
-				var index = jQuery('#'+settings.ListElement+' a').index(this);
-				stopAutoplay();
-				var ReverseIndex = Slides-index;
+      jQuery('#'+settings.ListElement+' a').bind('click', function(){
+        var index = jQuery('#'+settings.ListElement+' a').index(this);
+        stopAutoplay();
+        var ReverseIndex = Slides-index;
 
-				jumpTo(ReverseIndex);
+        jumpTo(ReverseIndex);
 
-				return false;
-			});
-		}
+        return false;
+      });
+    }
 
-		if(settings.PlayPauseElement){
-			if(!jQuery('#'+settings.PlayPauseElement).css('display')){
-				jQuery(this).after('<a href="#" id="'+settings.PlayPauseElement+'"><\/a>');
-			}
+    if(settings.PlayPauseElement){
+      if(!jQuery('#'+settings.PlayPauseElement).css('display')){
+        jQuery(this).after('<a href="#" id="'+settings.PlayPauseElement+'"><\/a>');
+      }
 
-			if(settings.autoplay){
-				jQuery('#'+settings.PlayPauseElement).html(settings.PauseText);
-			}else{
-				jQuery('#'+settings.PlayPauseElement).html(settings.PlayText);
-			}
+      if(settings.autoplay){
+        jQuery('#'+settings.PlayPauseElement).html(settings.PauseText);
+      }else{
+        jQuery('#'+settings.PlayPauseElement).html(settings.PlayText);
+      }
 
-			jQuery('#'+settings.PlayPauseElement).bind('click', function(){
-				if(intval){
-					stopAutoplay();
-				}else{
-					autoplay();
-				}
-				return false;
-			});
-		}
+      jQuery('#'+settings.PlayPauseElement).bind('click', function(){
+        if(intval){
+          stopAutoplay();
+        }else{
+          autoplay();
+        }
+        return false;
+      });
+    }
 
-		// start autoplay or set it to false
-		if(settings.autoplay){autoplay();}else{intval=false;}
-	});
+    // start autoplay or set it to false
+    if(settings.autoplay){autoplay();}else{intval=false;}
+  });
 };
